6545926a46cd5b4b06f56495799a62d0580c70fe
Mounia Belkheir <mouniabelkheir@outlook.com>
2023-09-25 12:13:47 +0100

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,19 @@ function App() {
     <div className="App">
       <Container header="Example header" direction="horizontal">
         <div>This is an example!</div>
+        {/* <div>This is an example!</div>
         <div>This is an example!</div>
         <div>This is an example!</div>
         <div>This is an example!</div>
         <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
+        <div>This is an example!</div> */}
       </Container>
       <Container header="Example header" textPosition="center">
         <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
         />
-        <img
+        {/* <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
         />
@@ -29,7 +29,7 @@ function App() {
         <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
+        /> */}
       </Container>
     </div>
   );
